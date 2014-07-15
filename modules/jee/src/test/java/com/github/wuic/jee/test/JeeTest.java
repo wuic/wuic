ffa566d7e4a526051ce7ca171b25338ae7bc672d
DROUET, Guillaume <guillaume.drouet@capgemini.com>
2014-07-15 23:03:10 +0200

    Adds JEE test

---
+++ modules/jee/src/test/java/com/github/wuic/jee/test/JeeTest.java
@@ -0,0 +1,108 @@
+/*
+ * "Copyright (c) 2014   Capgemini Technology Services (hereinafter "Capgemini")
+ *
+ * License/Terms of Use
+ * Permission is hereby granted, free of charge and for the term of intellectual
+ * property rights on the Software, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to use, copy, modify and
+ * propagate free of charge, anywhere in the world, all or part of the Software
+ * subject to the following mandatory conditions:
+ *
+ * -   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Any failure to comply with the above shall automatically terminate the license
+ * and be construed as a breach of these Terms of Use causing significant harm to
+ * Capgemini.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, PEACEFUL ENJOYMENT,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Capgemini shall not be used in
+ * advertising or otherwise to promote the use or other dealings in this Software
+ * without prior written authorization from Capgemini.
+ *
+ * These Terms of Use are subject to French law.
+ *
+ * IMPORTANT NOTICE: The WUIC software implements software components governed by
+ * open source software licenses (BSD and Apache) of which CAPGEMINI is not the
+ * author or the editor. The rights granted on the said software components are
+ * governed by the specific terms and conditions specified by Apache 2.0 and BSD
+ * licenses."
+ */
+
+
+package com.github.wuic.jee.test;
+
+import com.github.wuic.exception.wrapper.StreamException;
+import com.github.wuic.jee.WuicJeeContext;
+import com.github.wuic.nut.Nut;
+import com.github.wuic.nut.NutDao;
+import com.github.wuic.nut.jee.WebappNutDaoBuilder;
+import com.github.wuic.test.Server;
+import com.github.wuic.test.WuicConfiguration;
+import com.github.wuic.test.WuicRunnerConfiguration;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Tests for JEE related classes.
+ * </p>
+ *
+ * @author Guillaume DROUET
+ * @version 1.5
+ * @since 0.3.0
+ */
+@RunWith(JUnit4.class)
+@WuicRunnerConfiguration(webApplicationPath = "/jeeTest")
+public class JeeTest {
+
+    /**
+     * The server running during tests.
+     */
+    @ClassRule
+    public static com.github.wuic.test.Server server = new Server();
+
+
+    /**
+     * XML configuration.
+     */
+    @Rule
+    public WuicConfiguration configuration = new WuicConfiguration();
+
+    /**
+     * Tests that {@code ServletContext} is set.
+     */
+    @Test
+    public void contextTest() {
+        Assert.assertNotNull(WuicJeeContext.getWuicFacade());
+        Assert.assertNotNull(WuicJeeContext.getServletContext());
+    }
+
+    /**
+     * Tests {@link com.github.wuic.nut.jee.WebappNut} creation.
+     *
+     * @throws StreamException if nut creation fails
+     */
+    @Test
+    public void webappNutTest() throws StreamException {
+        final WebappNutDaoBuilder builder = new WebappNutDaoBuilder();
+        final NutDao dao = builder.build();
+        final List<Nut> nuts = dao.create("index.html");
+        Assert.assertNotNull(nuts);
+        Assert.assertEquals(1, nuts.size());
+        Assert.assertEquals("index.html", nuts.get(0).getName());
+
+    }
+}
