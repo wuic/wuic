739ed94485b080885ac093e1bc129d31bb233d19
gdrouet <guidrouet@gmail.com>
2014-09-10 21:28:56 +0200

    Adapts script to new repository structure.

--- javadoc.sh
+++ javadoc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd wuic && \
 mvn clean javadoc:aggregate -Pjavadoc && \
-git checkout gh-pages && \
+cd ../../wuic.github.io && \
 git rm -r apidocs && \
-cp -r target/site/apidocs . && \
+cp -r ../web-ui-compressor/wuic/target/site/apidocs . && \
 git add apidocs
