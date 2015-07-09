wuic/wuic

commit 0ec80964d8f798ea26126aead41852377245d0d2

Fixes #194.

wuic/core/src/main/java/com/github/wuic/nut/CompositeNut.java
@@ -55,8 +55,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
@@ -153,6 +155,23 @@ public class CompositeNut extends PipedConvertibleNut {
 
     /**
      * <p>
+     * Indicates if at least one nut of the composition has a transformer.
+     * </p>
+     *
+     * @return {@code true} is a transformer exists inside the composition, {@code false} otherwise
+     */
+    public boolean hasTransformers() {
+        for (final ConvertibleNut nut : compositionList) {
+            if (nut.getTransformers() != null) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * <p>
      * Indicates if version number is computed asynchronously.
      * </p>
      *
@@ -187,44 +206,44 @@ public class CompositeNut extends PipedConvertibleNut {
             }
 
             final Pipe<ConvertibleNut> finalPipe;
+            final boolean hasTransformers = hasTransformers();
 
             // Collect transformer executed for each nut and group transformers for aggregated content
-            if (getTransformers() != null) {
-                final List<Pipe.Transformer<ConvertibleNut>> separateStream = new ArrayList<Pipe.Transformer<ConvertibleNut>>();
+            if (hasTransformers) {
+                // Get transformers
                 final Set<Pipe.Transformer<ConvertibleNut>> aggregatedStream = new LinkedHashSet<Pipe.Transformer<ConvertibleNut>>();
+                final Map<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>> nuts = new LinkedHashMap<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>>();
+                populateTransformers(aggregatedStream, nuts);
 
-                for (final Pipe.Transformer<ConvertibleNut> transformer : getTransformers()) {
-                    if (transformer.canAggregateTransformedStream()) {
-                        separateStream.add(transformer);
-                    } else {
-                        aggregatedStream.add(transformer);
-                    }
-                }
-
-                final List<InputStream> is =
-                        new ArrayList<InputStream>(compositionList.size() * (streamSeparator == null ? 1 : NumberUtils.TWO));
+                final List<InputStream> is = new ArrayList<InputStream>(compositionList.size() * (streamSeparator == null ? 1 : NumberUtils.TWO));
 
                 // First we transform each nut with transformers producing content which could be aggregated
-                for (final ConvertibleNut nut : compositionList) {
+                for (final Map.Entry<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>> entry : nuts.entrySet()) {
                     final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    final Pipe<ConvertibleNut> pipe = new Pipe<ConvertibleNut>(new NutWrapper(this) {
 
-                        /**
-                         * {@inheritDoc}
-                         */
-                        @Override
-                        public String getName() {
-                            return nut.getInitialName();
+                    if (!entry.getValue().isEmpty()) {
+
+                        // We pass this composition in order to receive any referenced nut or whatever state change
+                        final Pipe<ConvertibleNut> pipe = new Pipe<ConvertibleNut>(new NutWrapper(this) {
+
+                            /**
+                             * {@inheritDoc}
+                             */
+                            @Override
+                            public String getName() {
+                                return entry.getKey().getName();
+                            }
+                        }, entry.getKey().openStream());
+
+                        for (final Pipe.Transformer<ConvertibleNut> transformer : entry.getValue()) {
+                            pipe.register(transformer);
                         }
-                    }, nut.openStream());
 
-                    for (final Pipe.Transformer<ConvertibleNut> transformer :separateStream) {
-                        pipe.register(transformer);
+                        Pipe.executeAndWriteTo(pipe, entry.getKey().getReadyCallbacks(), bos);
+                        is.add(new ByteArrayInputStream(bos.toByteArray()));
+                    } else {
+                        is.add(entry.getKey().openStream());
                     }
-
-                    Pipe.executeAndWriteTo(pipe, nut.getReadyCallbacks(), bos);
-
-                    is.add(new ByteArrayInputStream(bos.toByteArray()));
 
                     if (streamSeparator != null) {
                         is.add(new ByteArrayInputStream(streamSeparator));
@@ -252,8 +271,46 @@ public class CompositeNut extends PipedConvertibleNut {
      * {@inheritDoc}
      */
     @Override
+    public void addTransformer(final Pipe.Transformer<ConvertibleNut> transformer) {
+        compositionList.get(0).addTransformer(transformer);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public InputStream openStream() throws IOException {
         return new CompositeInputStream();
+    }
+
+    /**
+     * <p>
+     * Populates the given {@code Set} and {@code Map} with the transformers inside the composition.
+     * If the {@link com.github.wuic.util.Pipe.Transformer#canAggregateTransformedStream()} returns {@code true}, then
+     * it's added to a list associated to the nut with the given map. Otherwise the transformer will be added in the
+     * set specified in parameter.
+     * </p>
+     *
+     * @param aggregatedStream transformer that operate on aggregated streams
+     * @param nuts all nuts with their transformer that produce a stream that can be aggregated later
+     */
+    private void populateTransformers(final Set<Pipe.Transformer<ConvertibleNut>> aggregatedStream,
+                                      final Map<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>> nuts) {
+        for (final ConvertibleNut nut : compositionList) {
+            final List<Pipe.Transformer<ConvertibleNut>> separateStream = new ArrayList<Pipe.Transformer<ConvertibleNut>>();
+
+            if (nut.getTransformers() != null) {
+                for (final Pipe.Transformer<ConvertibleNut> transformer : nut.getTransformers()) {
+                    if (transformer.canAggregateTransformedStream()) {
+                        separateStream.add(transformer);
+                    } else {
+                        aggregatedStream.add(transformer);
+                    }
+                }
+            }
+
+            nuts.put(nut, separateStream);
+        }
     }
 
     /**

wuic/core/src/test/java/com/github/wuic/test/nut/CompositeNutTest.java
@@ -46,6 +46,9 @@ import com.github.wuic.nut.CompositeNut;
 import com.github.wuic.util.FutureLong;
 import com.github.wuic.util.CollectionUtils;
 import com.github.wuic.util.IOUtils;
+import com.github.wuic.util.NutUtils;
+import com.github.wuic.util.Pipe;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -53,7 +56,11 @@ import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * <p>
@@ -70,10 +77,10 @@ public class CompositeNutTest {
     /**
      * Nominal test.
      *
-     * @throws Exception if test fails
+     * @throws IOException if test fails
      */
     @Test
-    public void compositeTest() throws Exception {
+    public void compositeTest() throws IOException {
         final ConvertibleNut n1 = Mockito.mock(ConvertibleNut.class);
         Mockito.when(n1.openStream()).thenReturn(new ByteArrayInputStream("some css rules".getBytes()));
         Mockito.when(n1.getInitialNutType()).thenReturn(NutType.CSS);
@@ -93,5 +100,77 @@ public class CompositeNutTest {
         final Nut composite = new CompositeNut(true, "composite", null, Mockito.mock(ProcessContext.class),
                 ConvertibleNut[].class.cast(Arrays.asList(n1, n2).toArray()));
         IOUtils.copyStream(composite.openStream(), new ByteArrayOutputStream());
+    }
+
+    /**
+     * <p>
+     * Checks that transformer are correctly applied in a compisition.
+     * </p>
+     *
+     * @throws java.io.IOException if test fails
+     */
+    @Test
+    public void aggregationTransformTest() throws IOException {
+        final Pipe.Transformer<ConvertibleNut> t1 = new Pipe.Transformer<ConvertibleNut>() {
+            @Override
+            public void transform(InputStream is, OutputStream os, ConvertibleNut convertible) throws IOException {
+                IOUtils.copyStream(is, os);
+                os.write(" t1".getBytes());
+            }
+
+            @Override
+            public boolean canAggregateTransformedStream() {
+                return true;
+            }
+        };
+
+        final Pipe.Transformer<ConvertibleNut> t2 = new Pipe.Transformer<ConvertibleNut>() {
+            @Override
+            public void transform(InputStream is, OutputStream os, ConvertibleNut convertible) throws IOException {
+                IOUtils.copyStream(is, os);
+                os.write(" t2".getBytes());
+            }
+
+            @Override
+            public boolean canAggregateTransformedStream() {
+                return true;
+            }
+        };
+
+        final Pipe.Transformer<ConvertibleNut> t3 = new Pipe.Transformer<ConvertibleNut>() {
+            @Override
+            public void transform(InputStream is, OutputStream os, ConvertibleNut convertible) throws IOException {
+                IOUtils.copyStream(is, os);
+                os.write(" this is the end".getBytes());
+            }
+
+            @Override
+            public boolean canAggregateTransformedStream() {
+                return false;
+            }
+        };
+
+        final ConvertibleNut n1 = Mockito.mock(ConvertibleNut.class);
+        Mockito.when(n1.openStream()).thenReturn(new ByteArrayInputStream("some css rules".getBytes()));
+        Mockito.when(n1.getInitialNutType()).thenReturn(NutType.CSS);
+        Mockito.when(n1.getName()).thenReturn("n1.css");
+        Mockito.when(n1.getInitialName()).thenReturn("n1.css");
+        Mockito.when(n1.getReferencedNuts()).thenReturn(CollectionUtils.newList(Mockito.mock(ConvertibleNut.class)));
+        Mockito.when(n1.getVersionNumber()).thenReturn(new FutureLong(1L));
+        Mockito.when(n1.getTransformers()).thenReturn(new HashSet<Pipe.Transformer<ConvertibleNut>>(Arrays.asList(t1, t3)));
+        
+        final ConvertibleNut n2 = Mockito.mock(ConvertibleNut.class);
+        Mockito.when(n2.openStream()).thenReturn(new ByteArrayInputStream("other css rules".getBytes()));
+        Mockito.when(n2.getInitialNutType()).thenReturn(NutType.CSS);
+        Mockito.when(n2.getName()).thenReturn("n2.css");
+        Mockito.when(n2.getInitialName()).thenReturn("n2.css");
+        Mockito.when(n2.getReferencedNuts()).thenReturn(CollectionUtils.newList(Mockito.mock(ConvertibleNut.class)));
+        Mockito.when(n2.getVersionNumber()).thenReturn(new FutureLong(1L));
+        Mockito.when(n2.getTransformers()).thenReturn(new HashSet<Pipe.Transformer<ConvertibleNut>>(Arrays.asList(t2, t3)));
+
+        final ConvertibleNut composite = new CompositeNut(true, "composite", null, Mockito.mock(ProcessContext.class),
+                ConvertibleNut[].class.cast(Arrays.asList(n1, n2).toArray()));
+
+        Assert.assertEquals("some css rules t1other css rules t2 this is the end", NutUtils.readTransform(composite));
     }
 }

commit f067b8842bfa6322a676105419d86dc7d4875043

Fixes regression introduced by commit 0ec80964d8f798ea26126aead41852377245d0d2.
A map was storing ConvertibleNut as keys, but since some of them have the same name their equals() method returns true and the Map keeps only one of them. A simple fix was to switch the key with the value.

wuic/core/src/main/java/com/github/wuic/nut/CompositeNut.java
@@ -212,16 +212,16 @@ public class CompositeNut extends PipedConvertibleNut {
             if (hasTransformers) {
                 // Get transformers
                 final Set<Pipe.Transformer<ConvertibleNut>> aggregatedStream = new LinkedHashSet<Pipe.Transformer<ConvertibleNut>>();
-                final Map<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>> nuts = new LinkedHashMap<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>>();
+                final Map<List<Pipe.Transformer<ConvertibleNut>>, ConvertibleNut> nuts = new LinkedHashMap<List<Pipe.Transformer<ConvertibleNut>>, ConvertibleNut>();
                 populateTransformers(aggregatedStream, nuts);
 
                 final List<InputStream> is = new ArrayList<InputStream>(compositionList.size() * (streamSeparator == null ? 1 : NumberUtils.TWO));
 
                 // First we transform each nut with transformers producing content which could be aggregated
-                for (final Map.Entry<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>> entry : nuts.entrySet()) {
+                for (final Map.Entry<List<Pipe.Transformer<ConvertibleNut>>, ConvertibleNut> entry : nuts.entrySet()) {
                     final ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
-                    if (!entry.getValue().isEmpty()) {
+                    if (!entry.getKey().isEmpty()) {
 
                         // We pass this composition in order to receive any referenced nut or whatever state change
                         final Pipe<ConvertibleNut> pipe = new Pipe<ConvertibleNut>(new NutWrapper(this) {
@@ -231,18 +231,18 @@ public class CompositeNut extends PipedConvertibleNut {
                              */
                             @Override
                             public String getName() {
-                                return entry.getKey().getName();
+                                return entry.getValue().getName();
                             }
-                        }, entry.getKey().openStream());
+                        }, entry.getValue().openStream());
 
-                        for (final Pipe.Transformer<ConvertibleNut> transformer : entry.getValue()) {
+                        for (final Pipe.Transformer<ConvertibleNut> transformer : entry.getKey()) {
                             pipe.register(transformer);
                         }
 
-                        Pipe.executeAndWriteTo(pipe, entry.getKey().getReadyCallbacks(), bos);
+                        Pipe.executeAndWriteTo(pipe, entry.getValue().getReadyCallbacks(), bos);
                         is.add(new ByteArrayInputStream(bos.toByteArray()));
                     } else {
-                        is.add(entry.getKey().openStream());
+                        is.add(entry.getValue().openStream());
                     }
 
                     if (streamSeparator != null) {
@@ -295,7 +295,7 @@ public class CompositeNut extends PipedConvertibleNut {
      * @param nuts all nuts with their transformer that produce a stream that can be aggregated later
      */
     private void populateTransformers(final Set<Pipe.Transformer<ConvertibleNut>> aggregatedStream,
-                                      final Map<ConvertibleNut, List<Pipe.Transformer<ConvertibleNut>>> nuts) {
+                                      final Map<List<Pipe.Transformer<ConvertibleNut>>, ConvertibleNut> nuts) {
         for (final ConvertibleNut nut : compositionList) {
             final List<Pipe.Transformer<ConvertibleNut>> separateStream = new ArrayList<Pipe.Transformer<ConvertibleNut>>();
 
@@ -309,7 +309,7 @@ public class CompositeNut extends PipedConvertibleNut {
                 }
             }
 
-            nuts.put(nut, separateStream);
+            nuts.put(separateStream, nut);
         }
     }
 