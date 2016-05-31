wuic/wuic

commit 3837a6c0e0a6a354f9cf01ecf3c754281ae43731

Adds support for property placeholders.
A property in wuic.xml can be declared with ${...} notation in order to use a value declared elsewhere (the key needs to be specified in the bracket.
Inside the bracket, the property key can be followed by ":" and the default value to apply if no result is found.
Fixes #203.

wuic/servlet/src/main/java/com/github/wuic/servlet/WuicServletContextListener.java
@@ -51,6 +51,7 @@ import com.github.wuic.servlet.jetty.PathMap;
 import com.github.wuic.util.BiFunction;
 import com.github.wuic.util.Consumer;
 import com.github.wuic.util.IOUtils;
+import com.github.wuic.util.PropertyResolver;
 import com.github.wuic.util.UrlProvider;
 import com.github.wuic.util.UrlUtils;
 import com.github.wuic.util.WuicScheduledThreadPool;
@@ -70,7 +71,10 @@ import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.EnumSet;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
@@ -202,15 +206,15 @@ public class WuicServletContextListener implements ServletContextListener {
      * @return the function
      */
     @SuppressWarnings("unchecked")
-    public static BiFunction<String, String, String> getParamProvider(final ServletContext servletContext) {
+    public static PropertyResolver getParamProvider(final ServletContext servletContext) {
         final Object fct = servletContext.getAttribute(ApplicationConfig.INIT_PARAM_FUNCTION);
 
         if (fct == null) {
-            final String message = String.format("BiFunction is null, seems the %s was not initialized successfully.", WuicServletContextListener.class.getName());
+            final String message = String.format("PropertyResolver is null, seems the %s was not initialized successfully.", WuicServletContextListener.class.getName());
             WuicException.throwBadStateException(new IllegalArgumentException(message));
         }
 
-        return (BiFunction<String, String, String>) fct;
+        return (PropertyResolver) fct;
     }
 
     /**
@@ -219,9 +223,10 @@ public class WuicServletContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(final ServletContextEvent sce) {
         final ServletContext sc = sce.getServletContext();
-        final String paramClass = sc.getInitParameter(ApplicationConfig.INIT_PARAM_FUNCTION);
-        BiFunction<String, String, String> paramProvider;
         detectFilter(sc);
+
+        final String paramClass = sc.getInitParameter(ApplicationConfig.INIT_PARAM_FUNCTION);
+        PropertyResolver paramProvider;
 
         // No specific provider, use default
         if (paramClass == null) {
@@ -396,7 +401,7 @@ public class WuicServletContextListener implements ServletContextListener {
      * @throws Exception if class can't be instantiated
      */
     @SuppressWarnings("unchecked")
-    private BiFunction<String, String, String> newParamClassInstance(final String paramClass, final ServletContext sc)
+    private PropertyResolver newParamClassInstance(final String paramClass, final ServletContext sc)
             throws Exception {
         final Class<?> clazz = Class.forName(paramClass);
 
@@ -414,9 +419,9 @@ public class WuicServletContextListener implements ServletContextListener {
 
         // Check if constructor exists
         if (scConstructor != null) {
-            return (BiFunction<String, String, String>) scConstructor.newInstance(sc);
+            return (PropertyResolver) scConstructor.newInstance(sc);
         } else if (defaultConstructor != null) {
-            return (BiFunction<String, String, String>) defaultConstructor.newInstance();
+            return (PropertyResolver) defaultConstructor.newInstance();
         } else {
             throw new IllegalStateException(
                     String.format("'%s' provide at least a default constructor or a constructor expecting one parameter of type '%s'",
@@ -432,7 +437,7 @@ public class WuicServletContextListener implements ServletContextListener {
      * @param sc the servlet context
      * @return the function to apply
      */
-    protected BiFunction<String, String, String> paramProvider(final ServletContext sc) {
+    protected PropertyResolver paramProvider(final ServletContext sc) {
         return new InitParamProperties(sc);
     }
 
@@ -507,12 +512,12 @@ public class WuicServletContextListener implements ServletContextListener {
      * @author Guillaume DROUET
      * @since 0.5.2
      */
-    public final class PropertiesWrapper implements BiFunction<String, String, String> {
+    public final class PropertiesWrapper implements PropertyResolver {
 
         /**
          * Wraps the function.
          */
-        private final BiFunction<String, String, String> wrap;
+        private final PropertyResolver wrap;
 
         /**
          * Servlet context.
@@ -527,7 +532,7 @@ public class WuicServletContextListener implements ServletContextListener {
          * @param sc the servlet context
          * @param w  the function
          */
-        private PropertiesWrapper(final ServletContext sc, final BiFunction<String, String, String> w) {
+        private PropertiesWrapper(final ServletContext sc, PropertyResolver w) {
             wrap = w;
             servletContext = sc;
         }
@@ -536,8 +541,8 @@ public class WuicServletContextListener implements ServletContextListener {
          * {@inheritDoc}
          */
         @Override
-        public String apply(final String key, final String defaultVal) {
-            final String wrapResult = wrap.apply(key, defaultVal);
+        public String resolveProperty(final String key) {
+            final String wrapResult = wrap.resolveProperty(key);
 
             if (ApplicationConfig.WUIC_SERVLET_CONTEXT_PARAM.equals(key)) {
                 final ServletRegistration r = WuicServlet.findServletRegistration(servletContext);
@@ -551,7 +556,7 @@ public class WuicServletContextListener implements ServletContextListener {
                     // We expect a star in the mapping
                     if (star != -1) {
                         // The user has defined a context-param explicitly, it will be ignored and replace by servlet mapping
-                        if (wrapResult != null && !wrapResult.equals(defaultVal)) {
+                        if (wrapResult != null) {
                             WuicServletContextListener.this.log.warn(
                                     "WuicServlet is installed and its mapping will be used to resolve '{}' property. 'context-param' configured with value '{}' will be ignored.",
                                     ApplicationConfig.WUIC_SERVLET_CONTEXT_PARAM,
@@ -579,6 +584,14 @@ public class WuicServletContextListener implements ServletContextListener {
                 return wrapResult;
             }
         }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Collection<Object> getKeys() {
+            return wrap.getKeys();
+        }
     }
 
     /**
@@ -589,7 +602,7 @@ public class WuicServletContextListener implements ServletContextListener {
      * @author Guillaume DROUET
      * @since 0.5.0
      */
-    public static final class InitParamProperties implements BiFunction<String, String, String> {
+    public static final class InitParamProperties implements PropertyResolver {
 
         /**
          * The servlet context.
@@ -611,9 +624,22 @@ public class WuicServletContextListener implements ServletContextListener {
          * {@inheritDoc}
          */
         @Override
-        public String apply(final String key, final String defaultValue) {
-            final String retval = servletContext.getInitParameter(key);
-            return retval == null ? defaultValue : retval;
+        public String resolveProperty(final String key) {
+            return servletContext.getInitParameter(key);
+        }
+
+        /**
+         * '{@inheritDoc}
+         */
+        @Override
+        public Collection<Object> getKeys() {
+            final List<Object> retval = new ArrayList<Object>();
+
+            for (final Enumeration<String> enumeration = servletContext.getInitParameterNames(); enumeration.hasMoreElements();) {
+                retval.add(enumeration.nextElement());
+            }
+
+            return retval;
         }
     }
 }

wuic/core/src/main/java/com/github/wuic/WuicFacade.java
@@ -181,7 +181,7 @@ public final class WuicFacade implements ObjectBuilderInspector {
         // This inspector will configure any ClassPathResourceResolverHandler
         inspectors.add(this);
 
-        builder = new ContextBuilder(b.contextBuilder(), b.getWuicPropertiesPath(),
+        builder = new ContextBuilder(b.contextBuilder(), b.getPropertyResolver(),
                 inspectors.toArray(new ObjectBuilderInspector[inspectors.size()]));
 
         final ContextBuilderConfigurator[] array = new ContextBuilderConfigurator[config.getConfigurators().size()];

wuic/core/src/main/java/com/github/wuic/WuicFacadeBuilder.java
@@ -46,8 +46,10 @@ import com.github.wuic.exception.WorkflowTemplateNotFoundException;
 import com.github.wuic.exception.WuicException;
 import com.github.wuic.nut.HeapListener;
 import com.github.wuic.nut.Nut;
-import com.github.wuic.util.BiFunction;
+import com.github.wuic.util.EnhancedPropertyResolver;
 import com.github.wuic.util.IOUtils;
+import com.github.wuic.util.MapPropertyResolver;
+import com.github.wuic.util.PropertyResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,6 +60,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * <p>
@@ -90,11 +93,6 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
     private Boolean multipleConfigInTagSupport;
 
     /**
-     * The URL where wuic.properties file is located.
-     */
-    private URL wuicPropertiesPath;
-
-    /**
      * The URL where wuic.xml file is located.
      */
     private URL wuicXmlPath;
@@ -125,6 +123,11 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
     private final ContextBuilderFacade contextBuilder;
 
     /**
+     * The {@link com.github.wuic.util.PropertyResolver} that will be used by the builder to get properties.
+     */
+    private final EnhancedPropertyResolver propertyResolver;
+
+    /**
      * <p>
      * Builds an instance with default settings.
      * </p>
@@ -134,12 +137,13 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
         warmUpStrategy = WuicFacade.WarmupStrategy.NONE;
         multipleConfigInTagSupport = Boolean.TRUE;
         wuicXmlPath = getClass().getResource("/wuic.xml");
-        wuicPropertiesPath = getClass().getResource("/wuic.properties");
+        propertyResolver = new EnhancedPropertyResolver();
         useDefaultContextBuilderConfigurator = Boolean.TRUE;
         inspectors = new ArrayList<ObjectBuilderInspector>();
         configurators = new ArrayList<ContextBuilderConfigurator>();
         contextBuilder = new ContextBuilderFacade();
         classpathResourceResolver = this;
+        createResolverFromPath(getClass().getResource("/wuic.properties"));
     }
 
     /**
@@ -150,31 +154,44 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
      *
      * @param properties the properties
      */
-    public WuicFacadeBuilder(final BiFunction<String, String, String> properties) {
+    public WuicFacadeBuilder(final PropertyResolver properties) {
         this();
-        contextPath = properties.apply(ApplicationConfig.WUIC_SERVLET_CONTEXT_PARAM, "");
-
-        // Context where nuts will be exposed
-        final String wuicCp = IOUtils.mergePath("/", contextPath);
-
-        log.info("WUIC's full context path is {}", wuicCp);
+        propertyResolver.addPropertyResolver(properties);
 
         try {
-            final String multipleConfStr = properties.apply(ApplicationConfig.WUIC_SERVLET_MULTIPLE_CONG_IN_TAG_SUPPORT, "true");
+            // Choose specific location for property file
+            final String propertyPath = propertyResolver.resolveProperty(ApplicationConfig.WUIC_PROPERTIES_PATH_PARAM, null);
+
+            if (propertyPath != null) {
+                if (Boolean.parseBoolean(propertyResolver.resolveProperty(ApplicationConfig.WUIC_PROPERTIES_SYS_PROP_PARAM, "false"))) {
+                    createResolverFromPath(new URL(System.getProperty(propertyPath)));
+                } else {
+                    createResolverFromPath(new URL(propertyPath));
+                }
+            }
+
+            contextPath = propertyResolver.resolveProperty(ApplicationConfig.WUIC_SERVLET_CONTEXT_PARAM, "");
+
+            // Context where nuts will be exposed
+            final String wuicCp = IOUtils.mergePath("/", contextPath);
+
+            log.info("WUIC's full context path is {}", wuicCp);
+
+            final String multipleConfStr = propertyResolver.resolveProperty(ApplicationConfig.WUIC_SERVLET_MULTIPLE_CONG_IN_TAG_SUPPORT, "true");
 
             if (!Boolean.parseBoolean(multipleConfStr)) {
                 disableMultipleConfigInTagSupport();
             }
 
-            final String warmupStrategyStr = properties.apply(ApplicationConfig.WUIC_WARMUP_STRATEGY, WuicFacade.WarmupStrategy.NONE.name());
+            final String warmupStrategyStr = propertyResolver.resolveProperty(ApplicationConfig.WUIC_WARMUP_STRATEGY, WuicFacade.WarmupStrategy.NONE.name());
             final WuicFacade.WarmupStrategy warmupStrategy = WuicFacade.WarmupStrategy.valueOf(warmupStrategyStr);
 
-            final String xmlPath = properties.apply(ApplicationConfig.WUIC_SERVLET_XML_PATH_PARAM, null);
-            final String propertyPath = properties.apply(ApplicationConfig.WUIC_PROPERTIES_PATH_PARAM, null);
+            final String xmlPath = propertyResolver.resolveProperty(ApplicationConfig.WUIC_SERVLET_XML_PATH_PARAM, null);
+
             contextPath(wuicCp);
             warmUpStrategy(warmupStrategy);
 
-            final String useDefaultConfStr = properties.apply(ApplicationConfig.WUIC_USE_DEFAULT_CONTEXT_BUILDER_CONFIGURATORS, "true");
+            final String useDefaultConfStr = propertyResolver.resolveProperty(ApplicationConfig.WUIC_USE_DEFAULT_CONTEXT_BUILDER_CONFIGURATORS, "true");
 
             if (!Boolean.parseBoolean(useDefaultConfStr)) {
                 noDefaultContextBuilderConfigurator();
@@ -182,24 +199,15 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
 
             // Choose specific location for XML file
             if (xmlPath != null) {
-                if (Boolean.parseBoolean(properties.apply(ApplicationConfig.WUIC_SERVLET_XML_SYS_PROP_PARAM, "false"))) {
+                if (Boolean.parseBoolean(propertyResolver.resolveProperty(ApplicationConfig.WUIC_SERVLET_XML_SYS_PROP_PARAM, "false"))) {
                     wuicXmlPath(new URL(System.getProperty(xmlPath)));
                 } else {
                     wuicXmlPath(new URL(xmlPath));
                 }
             }
 
-            // Choose specific location for property file
-            if (propertyPath != null) {
-                if (Boolean.parseBoolean(properties.apply(ApplicationConfig.WUIC_PROPERTIES_SYS_PROP_PARAM, "false"))) {
-                    wuicPropertiesPath(new URL(System.getProperty(propertyPath)));
-                } else {
-                    wuicPropertiesPath(new URL(propertyPath));
-                }
-            }
-
-            additionalContextBuilderConfigurators(properties);
-            additionalObjectBuilderInspectors(properties);
+            additionalContextBuilderConfigurators();
+            additionalObjectBuilderInspectors();
         } catch (MalformedURLException mue) {
             WuicException.throwBadStateException(new IllegalStateException("Unable to initialize WuicFacade", mue));
         } catch (ClassNotFoundException cnfe) {
@@ -208,6 +216,30 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
             WuicException.throwBadStateException(ie);
         } catch (IllegalAccessException iae) {
             WuicException.throwBadStateException(iae);
+        }
+    }
+
+    /**
+     * <p>
+     * Creates a new {@link PropertyResolver} based on the properties at the given URL.
+     * </p>
+     *
+     * @param properties the properties URL
+     */
+    private void createResolverFromPath(final URL properties) {
+        if (properties != null) {
+            InputStream is = null;
+
+            try {
+                is = properties.openStream();
+                final Properties props = new Properties();
+                props.load(is);
+                propertyResolver.addPropertyResolver(new MapPropertyResolver(props));
+            } catch (IOException ioe) {
+                log.error("Unable to load properties", ioe);
+            } finally {
+                IOUtils.close(is);
+            }
         }
     }
 
@@ -230,15 +262,14 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
      * {@link ApplicationConfig#WUIC_ADDITIONAL_BUILDER_CONFIGURATORS} property.
      * </p>
      *
-     * @param properties the properties
      * @return this
      * @throws ClassNotFoundException if class does not exists
      * @throws IllegalAccessException if class can be instantiated
      * @throws InstantiationException if an error occurs during instantiation
      */
-    public final WuicFacadeBuilder additionalContextBuilderConfigurators(final BiFunction<String, String, String> properties)
+    public final WuicFacadeBuilder additionalContextBuilderConfigurators()
             throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        final String value = properties.apply(ApplicationConfig.WUIC_ADDITIONAL_BUILDER_CONFIGURATORS, "");
+        final String value = propertyResolver.resolveProperty(ApplicationConfig.WUIC_ADDITIONAL_BUILDER_CONFIGURATORS, "");
 
         if (!value.isEmpty()) {
             final String[] classes = value.split(",");
@@ -260,15 +291,14 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
      * {@link ApplicationConfig#WUIC_ADDITIONAL_BUILDER_INSPECTOR} property.
      * </p>
      *
-     * @param properties the properties
      * @return this
      * @throws ClassNotFoundException if class does not exists
      * @throws IllegalAccessException if class can be instantiated
      * @throws InstantiationException if an error occurs during instantiation
      */
-    public final WuicFacadeBuilder additionalObjectBuilderInspectors(final BiFunction<String, String, String> properties)
+    public final WuicFacadeBuilder additionalObjectBuilderInspectors()
             throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        final String value = properties.apply(ApplicationConfig.WUIC_ADDITIONAL_BUILDER_INSPECTOR, "");
+        final String value = propertyResolver.resolveProperty(ApplicationConfig.WUIC_ADDITIONAL_BUILDER_INSPECTOR, "");
 
         if (!value.isEmpty()) {
             final String[] classes = value.split(",");
@@ -306,7 +336,7 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
      * @return this
      */
     public final WuicFacadeBuilder wuicPropertiesPath(final URL properties) {
-        this.wuicPropertiesPath = properties;
+        createResolverFromPath(properties);
         return this;
     }
 
@@ -495,13 +525,13 @@ public class WuicFacadeBuilder implements ClassPathResourceResolver {
 
     /**
      * <p>
-     * Gets the wuic.properties location if any.
+     * Gets the property resolver.
      * </p>
      *
-     * @return the wuic.properties path
+     * @return the property resolver
      */
-    URL getWuicPropertiesPath() {
-        return wuicPropertiesPath;
+    PropertyResolver getPropertyResolver() {
+        return propertyResolver;
     }
 
     /**

wuic/core/src/main/java/com/github/wuic/context/ContextBuilder.java
@@ -62,14 +62,14 @@ import com.github.wuic.nut.filter.NutFilter;
 import com.github.wuic.nut.filter.NutFilterHolder;
 import com.github.wuic.nut.filter.NutFilterService;
 import com.github.wuic.util.CollectionUtils;
-import com.github.wuic.util.IOUtils;
+import com.github.wuic.util.EnhancedPropertyResolver;
 import com.github.wuic.util.NumberUtils;
+import com.github.wuic.util.PropertyResolver;
+import com.github.wuic.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -78,7 +78,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
-import java.util.Properties;
 import java.util.ServiceLoader;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -146,9 +145,9 @@ public class ContextBuilder extends Observable {
     private ObjectBuilderFactory<NutFilter> nutFilterBuilderFactory;
 
     /**
-     * A property file URL containing properties to apply to all registered components.
+     * The property resolver.
      */
-    private URL propertySet;
+    private PropertyResolver propertyResolver;
 
     /**
      * Indicates that {@link #configureDefault()} has been called and default entries are injected.
@@ -164,9 +163,9 @@ public class ContextBuilder extends Observable {
      * @param properties the property set to apply to all components
      * @param inspectors the inspectors to add to the factories
      */
-    public ContextBuilder(final ContextBuilder b, final URL properties, final ObjectBuilderInspector ... inspectors) {
+    public ContextBuilder(final ContextBuilder b, final PropertyResolver properties, final ObjectBuilderInspector ... inspectors) {
         this(b.getEngineBuilderFactory(), b.getNutDaoBuilderFactory(), b.getNutFilterBuilderFactory(), false, inspectors);
-        propertySet = properties;
+        propertyResolver = properties;
     }
 
     /**
@@ -205,6 +204,7 @@ public class ContextBuilder extends Observable {
         this.taggedSettings = new TaggedSettings();
         this.lock = new ReentrantLock();
         this.configureDefault = false;
+        this.propertyResolver = new EnhancedPropertyResolver();
 
         this.engineBuilderFactory = engineBuilderFactory == null ?
                 new ObjectBuilderFactory<Engine>(EngineService.class, EngineService.DEFAULT_SCAN_PACKAGE) : engineBuilderFactory;
@@ -1471,6 +1471,18 @@ public class ContextBuilder extends Observable {
 
     /**
      * <p>
+     * Injects the placeholders in the given value thanks to the configured resolver.
+     * </p>
+     *
+     * @param value the value
+     * @return the injected value
+     */
+    public String injectPlaceholder(final String value) {
+        return StringUtils.injectPlaceholders(value, propertyResolver);
+    }
+
+    /**
+     * <p>
      * Inner class to configure a filter builder.
      * </p>
      *
@@ -2141,17 +2153,6 @@ public class ContextBuilder extends Observable {
 
     /**
      * <p>
-     * Applies all properties in the given parameter that are supported by any component configured in this builder.
-     * </p>
-     *
-     * @param properties the properties object
-     */
-    public void applyProperties(final Properties properties) {
-        taggedSettings.applyProperties(properties);
-    }
-
-    /**
-     * <p>
      * Builds the context. Should throws an {@link IllegalStateException} if the context is not correctly configured.
      * For instance: associate a heap to an undeclared {@link com.github.wuic.nut.dao.NutDao} builder ID.
      * </p>
@@ -2170,17 +2171,8 @@ public class ContextBuilder extends Observable {
                 lock.lock();
             }
 
-            if (propertySet != null) {
-                InputStream is = null;
-
-                try {
-                    is = propertySet.openStream();
-                    final Properties properties = new Properties();
-                    properties.load(is);
-                    applyProperties(properties);
-                } finally {
-                    IOUtils.close(is);
-                }
+            if (propertyResolver != null) {
+                taggedSettings.applyProperties(propertyResolver);
             }
 
             final Map<String, NutFilter> filterMap = taggedSettings.getFilterMap();

wuic/core/src/main/java/com/github/wuic/context/TaggedSettings.java
@@ -54,6 +54,7 @@ import com.github.wuic.nut.NutsHeap;
 import com.github.wuic.nut.dao.NutDao;
 import com.github.wuic.nut.filter.NutFilter;
 import com.github.wuic.util.CollectionUtils;
+import com.github.wuic.util.PropertyResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,7 +64,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.regex.Pattern;
 
 /**
@@ -598,13 +598,13 @@ public class TaggedSettings {
      * Applies the given property object to all registered components.
      * </p>
      *
-     * @param properties the property object
+     * @param properties the property resolver object
      * @see TaggedSettings#applyProperty(String, String)
      */
-    void applyProperties(final Properties properties) {
-        for (final Object key : properties.keySet()) {
+    void applyProperties(final PropertyResolver properties) {
+        for (final Object key : properties.getKeys()) {
             final String property = key.toString();
-            applyProperty(property, properties.getProperty(property));
+            applyProperty(property, properties.resolveProperty(property));
         }
     }
 

commit aebe21817393003b01a937bb85dcdf11f46592d8

Fixes regression introduced by commit 3837a6c.

wuic/servlet/src/main/java/com/github/wuic/servlet/WuicServletContextListener.java
@@ -579,7 +579,7 @@ public class WuicServletContextListener implements ServletContextListener {
                     retval = wrapResult;
                 }
 
-                return IOUtils.mergePath(servletContext.getContextPath(), retval);
+                return retval == null ? servletContext.getContextPath() : IOUtils.mergePath(servletContext.getContextPath(), retval);
             } else {
                 return wrapResult;
             }